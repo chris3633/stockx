d42d0454228ae91cda49af00d7d4b4d0a7b7cc6f
Christian <christianmorini36@gmail.com>
2021-01-22 19:28:21 +0100

    Cambiata navbar, aggiunto route alla dashboard dopo login, aggiunto logo navbar cliccabile.

--- src/components/App.js
+++ src/components/App.js
@@ -6,9 +6,11 @@ import Sidebar from "./Sidebar"
 
 import about from '../pages/AboutPage'
 import contacts from '../pages/ContactPage'
-import NavigationBar from './NavigationBar'
+import NavigationBar from './OldNavigationBar'
 import home from '../pages/HomePage'
+import dashboard from './Dashboard'
 
+import NavbarTop from './NavbarTop'
 import Login from './Login'
 import Signup from './Register'
 import ForgotPassword from './ForgotPassword'
@@ -20,11 +22,13 @@ function App() {
     <>
       <Router>
         {/* <Sidebar /> */}
-          <NavigationBar/>
+         {/* <NavigationBar/> */}
+         <NavbarTop />
           <Switch>
             <Route path='/' exact component={home} />
             <Route path='/about' exact component={about} />
             <Route path='/contacts' exact component={contacts} />
+            <Route path='/dashboard' exact component={dashboard} />
 
             <Route path="/register" exact component={Signup} />
             <Route path="/login" exact component={Login} />
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,6 +2,7 @@ import React, { useState } from "react"
 import { Card, Button, Alert } from "react-bootstrap"
 import { useAuth } from "../contexts/AuthContext"
 import { Link, useHistory } from "react-router-dom"
+import { Container } from "react-bootstrap"
 
 export default function Dashboard() {
   const [error, setError] = useState("")
@@ -21,21 +22,27 @@ export default function Dashboard() {
 
   return (
     <>
-      {/* <Card>
-        <Card.Body>
-          <h2 className="text-center mb-4">Profile</h2>
-          {error && <Alert variant="danger">{error}</Alert>}
-          <strong>Email:</strong> {currentUser.email}
-          <Link to="/update-profile" className="btn btn-primary w-100 mt-3">
-            Update Profile
+      <Container
+        className="d-flex align-items-center justify-content-center"
+        style={{ minHeight: "100vh" }}
+      >
+        <Card>
+          <Card.Body>
+            <h2 className="text-center mb-4">Profile</h2>
+            {error && <Alert variant="danger">{error}</Alert>}
+            <strong>Email:</strong> {currentUser.email}
+            <Link to="/update-profile" className="btn btn-primary w-100 mt-3">
+              Update Profile
           </Link>
-        </Card.Body>
-      </Card>
-      <div className="w-100 text-center mt-2">
-        <Button variant="link" onClick={handleLogout}>
-          Log Out
+          </Card.Body>
+        </Card>
+        <div className="w-100 text-center mt-2">
+          <Button variant="link" onClick={handleLogout}>
+            Log Out
         </Button>
-      </div>  */}
+        </div>
+
+      </Container>
     </>
   )
 }
