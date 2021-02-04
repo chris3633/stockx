a1fa4df07acfa1febf9ec988dcbc17f41c6581ed
SamueleDallospedale <samuele.dallospedale@gmail.com>
2021-02-04 02:26:39 +0100

    ora finalmente l'update dei dati dovrebbe funzionare, sono necessarie ancora migliorie come alert o controlli

---
+++ src/components/HandleUpdateProfile.js
@@ -0,0 +1,101 @@
+import firebase from 'firebase'
+import { Alert } from 'react-bootstrap';
+
+
+export default function handleUpdate(userEmail, name, surname, addressRef, cityRef, zipCodeRef, operations) {
+
+    var userRef = firebase.database().ref('users/' + window.btoa(userEmail))
+  
+    if (addressRef !== '' && cityRef !== '' && zipCodeRef !== '') {
+        userRef.set({
+            name: name,
+            surname: surname,
+            address: addressRef,
+            city: cityRef,
+            zipCode: zipCodeRef,
+            orders: operations
+        })
+
+    }
+
+    return true
+}
+
+/* const handleSubmit(addressRef, cityRef, zipCodeRef,operations)=> {
+    e.preventDefault()
+    console.log(addressRef)
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      return setError("Passwords do not match")
+    }
+    console.log('3')
+    const promises = []
+    setLoading(true)
+    setError("")
+
+    if (passwordRef.current.value) {
+      promises.push(updatePassword(passwordRef.current.value))
+      console.log('4')
+    }
+    if (addressRef !== '' || cityRef !== '' || zipCodeRef !== '') {
+      setLoading(true)
+      console.log('6')
+
+      console.log(addressRef)
+      console.log(cityRef)
+      /* if (addressRef !== address) {//non funziona perché sovrascrive
+        userRef.set({
+          address: "via",
+        })
+      }
+      if (cityRef !== city) {//non funziona perché al campo city aggiunge un figlio city con contenuto
+        userRef.child('city').set({
+          city: cityRef
+        })
+      }
+      if (zipCodeRef !== zipCode) {
+        userRef.child('zipCode').set({
+          zipCode: zipCodeRef
+        })
+      }
+      setLoading(false)
+    }
+
+    Promise.all(promises)
+      .then(() => {
+        console.log('5')
+        history.push("/")
+      })
+      .catch(() => {
+        setError("Failed to update account")
+      })
+      .finally(() => {
+        setLoading(false)
+      }) */
+
+/* function handleUpdate(addressRef, cityRef, zipCodeRef,operations) {
+  // A post entry.
+  setLoading(true)
+  console.log('6')
+
+  console.log(addressRef)
+  console.log(cityRef)
+  /* if (addressRef !== address) {//non funziona perché sovrascrive
+    userRef.set({
+      address: "via",
+    })
+  }
+  if (cityRef !== city) {//non funziona perché al campo city aggiunge un figlio city con contenuto
+    userRef.child('city').set({
+      city: cityRef
+    })
+  }
+  if (zipCodeRef !== zipCode) {
+    userRef.child('zipCode').set({
+      zipCode: zipCodeRef
+    })
+  }
+  setLoading(false)
+  return true
+}
+
+} */
--- src/components/UpdateProfile.js
+++ src/components/UpdateProfile.js
@@ -3,175 +3,158 @@ import { Form, Button, Card, Alert, Container } from "react-bootstrap"
 import { useAuth } from "../contexts/AuthContext"
 import { Link, useHistory } from "react-router-dom"
 import firebase from 'firebase'
+import handleUpdate from './HandleUpdateProfile'
 
 
 export default function UpdateProfile() {
   const passwordRef = useRef()
   const passwordConfirmRef = useRef()
-  
+  const addressRef = useRef()
+  const cityRef = useRef()
+  const zipCodeRef = useRef()
+
   const { currentUser, updatePassword } = useAuth()
   const [error, setError] = useState("")
   const [loading, setLoading] = useState(false)
   const history = useHistory()
-  
+
   var name
   var surname
   var address
   var city
   var zipCode
+  var operations
+  var userRef = firebase.database().ref('users/' + window.btoa(currentUser.email))
 
-  var userRef = firebase.database().ref('users/'+ window.btoa(currentUser.email))
-  
-  function getValues(){
-  userRef.child('name').once('value', (snapshot) => {
+  userRef.child('name').on('value', (snapshot) => {
     name = snapshot.val();
   });
-  userRef.child('surname').once('value', (snapshot) => {
+  userRef.child('surname').on('value', (snapshot) => {
     surname = snapshot.val();
   });
-  userRef.child('address').once('value', (snapshot) => {
+  userRef.child('address').on('value', (snapshot) => {
     address = snapshot.val();
   });
-  userRef.child('city').once('value', (snapshot) => {
+  userRef.child('city').on('value', (snapshot) => {
     city = snapshot.val();
   });
-  userRef.child('zipCode').once('value', (snapshot) => {
+  userRef.child('zipCode').on('value', (snapshot) => {
     zipCode = snapshot.val();
   });
-  return [name,surname,address,city,zipCode];
-}
+  userRef.child('orders').on('value', (snapshot) => {
+    operations = snapshot.val();
+  });
 
-  
-  var addressRef
-  var cityRef
-  var zipCodeRef
-
-  function handleUpdate() {
-    // A post entry.
-    addressRef=document.getElementById('address').value
-    cityRef=document.getElementById('city').value
-    zipCodeRef=document.getElementById('zipCode').value
-    console.log(addressRef)
-    console.log(cityRef)
-    console.log(zipCodeRef)
-    userRef.set({
-      address: addressRef,
-      city: cityRef,
-      zipCode: zipCodeRef
-    })
-  }
-
-  
-  function handleSubmit(e) {
-    e.preventDefault()
+  function handleSubmit() {
+    //e.preventDefault()
+    console.log(addressRef.current.value)
     if (passwordRef.current.value !== passwordConfirmRef.current.value) {
       return setError("Passwords do not match")
-    }
-
+    } 
+    
     const promises = []
     setLoading(true)
     setError("")
 
-    
     if (passwordRef.current.value) {
       promises.push(updatePassword(passwordRef.current.value))
     }
-    if (addressRef !== address || cityRef !== city || zipCodeRef !== zipCode) {
-      promises.push(handleUpdate(addressRef, cityRef, zipCodeRef))
+    if(addressRef.current.value && cityRef.current.value && zipCodeRef.current.value){
+      promises.push(handleUpdate(currentUser.email,name,surname,addressRef.current.value, cityRef.current.value, zipCodeRef.current.value, operations))
     }
 
     Promise.all(promises)
       .then(() => {
-        history.push("/dashboard")
+        history.push("/")
       })
       .catch(() => {
         setError("Failed to update account")
       })
       .finally(() => {
         setLoading(false)
-      })
-      
-    
+      }) 
+      setLoading(false)
   }
+
 
   return (
     <>
-    <div className="d-flex align-items-center justify-content-center"
+      <div className="d-flex align-items-center justify-content-center"
         style={{ minHeight: "100vh" }}>
-      <Card className="w-100" style={{ maxWidth: "400px" }}>
-        <Card.Body>
-          <h2 className="text-center mb-4">Update Profile</h2>
-          {error && <Alert variant="danger">{error}</Alert>}
-          <Form onSubmit={handleSubmit}>
-            <Form.Group id="email">
-              <Form.Label>Email</Form.Label>
-              <Form.Control
-                type="email"
-                disabled='true'
-                defaultValue={currentUser.email}
-              />
-            </Form.Group>
-            <Form.Group id="name">
-              <Form.Label>Name</Form.Label>
-              <Form.Control
-                disabled={true}
-                defaultValue={name}
-              />
-            </Form.Group>
-            <Form.Group id="surname">
-              <Form.Label>Surname</Form.Label>
-              <Form.Control
-                disabled={true}
-                defaultValue={surname}
-              />
-            </Form.Group>
-            <Form.Group id="address">
-              <Form.Label>Address</Form.Label>
-              <Form.Control
-                defaultValue={getValues()[2]}
-                ref={addressRef}
-              />
-            </Form.Group>
-            <Form.Group id="city">
-              <Form.Label>City</Form.Label>
-              <Form.Control
-                defaultValue={getValues()[3]}
-                ref={cityRef}
-              />
-            </Form.Group>
-            <Form.Group id="zipCode">
-              <Form.Label>Zip code</Form.Label>
-              <Form.Control
-                defaultValue={getValues()[4]}
-                ref={zipCodeRef}
-              />
-            </Form.Group>
-            <Form.Group id="password">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                ref={passwordRef}
-                placeholder="Leave blank to keep the same"
-              />
-            </Form.Group>
-            <Form.Group id="password-confirm">
-              <Form.Label>Password Confirmation</Form.Label>
-              <Form.Control
-                type="password"
-                ref={passwordConfirmRef}
-                placeholder="Leave blank to keep the same"
-              />
-            </Form.Group>
-            <Button onClick={()=>handleSubmit} disabled={loading} className="w-100" type="submit">
-              Update
+        <Card className="w-100" style={{ maxWidth: "400px" }}>
+          <Card.Body>
+            <h2 className="text-center mb-4">Update Profile</h2>
+            {error && <Alert variant="danger">{error}</Alert>}
+            <Form >
+              <Form.Group id="email">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  type="email"
+                  disabled={true}
+                  value={currentUser.email}
+                />
+              </Form.Group>
+              <Form.Group id="name">
+                <Form.Label>Name</Form.Label>
+                <Form.Control
+                  disabled={true}
+                  defaultValue={name}
+                />
+              </Form.Group>
+              <Form.Group id="surname">
+                <Form.Label>Surname</Form.Label>
+                <Form.Control
+                  disabled={true}
+                  defaultValue={surname}
+                />
+              </Form.Group>
+              <Form.Group id="address">
+                <Form.Label>Address</Form.Label>
+                <Form.Control
+                  defaultValue={address}
+                  ref={addressRef}
+                />
+              </Form.Group>
+              <Form.Group id="city">
+                <Form.Label>City</Form.Label>
+                <Form.Control
+                  defaultValue={city}
+                  ref={cityRef}
+                />
+              </Form.Group>
+              <Form.Group id="zipCode">
+                <Form.Label>Zip code</Form.Label>
+                <Form.Control
+                  defaultValue={zipCode}
+                  ref={zipCodeRef}
+                />
+              </Form.Group>
+              <Form.Group id="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  ref={passwordRef}
+                  placeholder="Leave blank to keep the same"
+                />
+              </Form.Group>
+              <Form.Group id="password-confirm">
+                <Form.Label>Password Confirmation</Form.Label>
+                <Form.Control
+                  type="password"
+                  ref={passwordConfirmRef}
+                  placeholder="Leave blank to keep the same"
+                />
+              </Form.Group>
+              <Button onClick={()=>handleSubmit()} disabled={loading} className="w-100" >{/*={() => handleUpdate(currentUser,addressRef.current.value, cityRef.current.value, zipCodeRef.current.value, operations,passwordRef,passwordConfirmRef)}*/}
+                Update
             </Button>
-          </Form>
-          <div className="w-100 text-center mt-2">
-        <Link to="/dashboard">Cancel</Link>
-      </div>
-        </Card.Body>
-      </Card>
-      
+            </Form>
+            <div className="w-100 text-center mt-2">
+              <Link to="/dashboard">Cancel</Link>
+            </div>
+          </Card.Body>
+        </Card>
+
       </div>
     </>
   )
