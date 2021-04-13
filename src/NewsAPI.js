adb203fbebbe464a10037284dbd7f8cc0f641dc4
chris3633 <66468954+chris3633@users.noreply.github.com>
2021-04-14 00:31:10 +0200

    Prima pulizia codice (eliminazione commenti e reformat code)

--- src/NewsAPI.js
+++ src/NewsAPI.js
@@ -1,14 +1,3 @@
-/* export const getLatestArticles = async () => {
-  const response = await fetch(
-     `https://newsapi.org/v2/top-headlines?country=us&sortBy=publishedAt&apiKey=${process.env.REACT_APP_NEWS_API_KEY}`
-
-  );
-  const json = await response.json();
-  return json;
-
-}; */
-
-
 
 export const getLatestArticles = async () => {
   const response = await fetch("https://cnbc.p.rapidapi.com/news/list-trending", {
@@ -17,9 +6,9 @@ export const getLatestArticles = async () => {
       "x-rapidapi-key": process.env.REACT_APP_NEWS_API_KEY,
       "x-rapidapi-host": "cnbc.p.rapidapi.com"
     }
-  }) 
+  })
   const json = await response.json();
   console.log(json);
-  return json; 
+  return json;
 
 };
--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,8 @@
-/* import firebase from "firebase/app"
-import "firebase/auth"
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 
-const app = firebase.initializeApp({
+const config = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
@@ -9,32 +10,17 @@ const app = firebase.initializeApp({
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-})
-
-export const auth = app.auth()
-export default app */
-
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/database';
- 
-const config = { apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID, };
+};
 
 class Firebase {
-  start= firebase.initializeApp(config);
+  start = firebase.initializeApp(config);
   constructor() {
     this.auth = firebase.auth();
     this.db = firebase.database();
   }
   user = uid => this.db.ref(`users/${uid}`);
- 
+
   users = () => this.db.ref('users');
 }
-export const auth=firebase.initializeApp(config).auth();
+export const auth = firebase.initializeApp(config).auth();
 export default Firebase;
